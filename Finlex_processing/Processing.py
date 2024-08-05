ea85ed991a83878acf3da9d630d074926baff330
AdrienCjust <adrien.cantin@ladyjustice4-0.com>
2024-08-05 11:41:19 +0300

    adding new files + a README

--- Finlex_processing/Processing.py
+++ Finlex_processing/Processing.py
@@ -31,6 +31,7 @@ def collect_text(tag_name, lst_of_content):
 		 
 def show_xml_info(xml_file, output_csv):
 	"""Load and Analysze the XML files
+	The function retrieve the content of some tags that are define throughout the function.
 	Arguments : 
 	output_csv : The csv that have been previously created, must be str
 	xml_file : The xml file  with the data, we want to extract
@@ -38,6 +39,7 @@ def show_xml_info(xml_file, output_csv):
 
 	tree = ET.parse(xml_file)
 	root = tree.getroot()
+	# THe namespaces help to structure the research by element
 	namespaces = {
 			'vah': 'http://www.vn.fi/skeemat/vahvistettavalaki/2010/04/27',
 			'sdk': 'http://www.finlex.fi/skeemat/edita/sdk/2010/04/27',
@@ -66,13 +68,13 @@ def show_xml_info(xml_file, output_csv):
 		if identification is not None:
 			document_type = identification.find('met1:AsiakirjatyyppiNimi', namespaces)
 			if document_type is not None:
-					document_type_text = document_type.text
-					print(f'Document Type: {document_type_text}')
-			
+				document_type_text = document_type.text
+				print(f'Document Type: {document_type_text}')
+
 			eduskunta_id = identification.find('asi:EduskuntaTunniste/asi1:AsiakirjaNroTeksti', namespaces)
 			if eduskunta_id is not None:
-					eduskunta_id_text = eduskunta_id.text
-					print(f'Eduskunta ID: {eduskunta_id_text}')
+				eduskunta_id_text = eduskunta_id.text
+				print(f'Eduskunta ID: {eduskunta_id_text}')
 			#for the date
 			date_id = identification.find('asi:EduskuntaTunniste/asi1:ValtiopaivavuosiTeksti', namespaces)
 			if date_id is not None:
@@ -83,94 +85,95 @@ def show_xml_info(xml_file, output_csv):
 		saados = root.find('saa:SaadosOsa/saa:Saados', namespaces)
 		lst_of_signatories = []
 		if saados is not None:
-				law_title = saados.find('saa:SaadosNimeke/saa:SaadosNimekeKooste', namespaces)
-				law_title_text = law_title.text if law_title is not None else 'N/A'
-				print(f'Law Title: {law_title_text}')
+			law_title = saados.find('saa:SaadosNimeke/saa:SaadosNimekeKooste', namespaces)
+			law_title_text = law_title.text if law_title is not None else 'N/A'
+			print(f'Law Title: {law_title_text}')
 
-				#extract the signatories of the document
-				signatures = root.find('asi:AllekirjoitusOsa', namespaces)
-				if signatures is not None:
-					for signer in signatures.findall('asi:Allekirjoittaja', namespaces):
-						position = signer.find('org:Henkilo/org1:AsemaTeksti', namespaces)
-						name = signer.find('org:Henkilo/org1:SukuNimi', namespaces)
-						
-						position_text = position.text if position is not None else 'N/A'
-						name_text = name.text if name is not None else 'N/A'
-
-						lst_of_signatories.append([position_text,name_text])
-						lst_of_signatories = remove_newlines(lst_of_signatories)
-						#The list will be like this  [[postion_first, name_first], [position_second,name_second]]
-						print(f'{position_text}: {name_text}')
-				else:
-					lst_of_signatories = [['N/A','N/A'],['N/A','N/A']]
-				
-				#Extract the section of the law
-				sections = saados.findall('saa:Pykalisto/saa:Pykala', namespaces)
-				if sections ==[]:
-					sections = saados.findall('saa:Pykalisto/saa:Luku/saa:Pykala', namespaces)
-				#print(sections)
-				for section in sections:
-					#Search all the information about a section
-		
-					section_id = section.get('{http://www.vn.fi/skeemat/saadoselementit/2010/04/27}identifiointiTunnus')
-					section_title = section.find('saa:SaadosOtsikkoKooste', namespaces)
-					section_title_text = section_title.text if section_title is not None else 'N/A'
-				
-					#Concat the content of the tag MomenttiKooste
-					section_content_parts = []
-					momentti_kooste = section.findall('saa:MomenttiKooste', namespaces)
-					collect_text(momentti_kooste, section_content_parts)
+			#extract the signatories of the document
+			signatures = root.find('asi:AllekirjoitusOsa', namespaces)
+			if signatures is not None:
+				for signer in signatures.findall('asi:Allekirjoittaja', namespaces):
+					position = signer.find('org:Henkilo/org1:AsemaTeksti', namespaces)
+					name = signer.find('org:Henkilo/org1:SukuNimi', namespaces)
 					
-					khodat_momentti = section.findall('saa:KohdatMomentti', namespaces)
-					for division in khodat_momentti:
-						momentti_johdanto_kooste = division.findall('saa:MomenttiJohdantoKooste', namespaces)
-						collect_text(momentti_johdanto_kooste , section_content_parts)
-					
-						momentti_kohta_kooste = division.findall('saa:MomenttiKohtaKooste', namespaces)
-						for momentti in momentti_kohta_kooste:
-							if momentti is not None and momentti.text is not None:
-								print(f'{momentti.text} Ceci est un texte')
-								text = momentti.text
-								section_content_parts.append(str(text).strip())	
-								print(section_content_parts)
-						#get the data in Luku Tag
-				
-										# Extract sections and chapters
-					for luku in saados.findall('saa:Luku', namespaces):
-							luku_id = luku.get('{http://www.vn.fi/skeemat/saadoselementit/2010/04/27}identifiointiTunnus', 'N/A')
-							luku_title = luku.findtext('saa:SaadosOtsikkoKooste', default='N/A', namespaces=namespaces)
+					position_text = position.text if position is not None else 'N/A'
+					name_text = name.text if name is not None else 'N/A'
 
-							for section in luku.findall('saa:Pykala', namespaces):
-									section_id = section.get('{http://www.vn.fi/skeemat/saadoselementit/2010/04/27}identifiointiTunnus', 'N/A')
-									section_title_text = section.findtext('saa:SaadosOtsikkoKooste', default='N/A', namespaces=namespaces)
-									
-									# Concat the content of the tag MomenttiKooste
-									#section_content_parts = []
-									momentti_kooste = section.findall('saa:MomenttiKooste', namespaces)
-									collect_text(momentti_kooste, section_content_parts)
-									#Test print
-									print(chapter_content_parts, "zehtgequirghvsudgfhestuhgbudfghvdufhbqiufdhg")	   
-					section_content_text = ' '.join(section_content_parts) if section_content_parts else 'N/A'
-					print(type(section_content_text))
-					print(section_content_text)
-					section_content_text =  section_content_text.replace(";", "")
-					#Sometimes there is no section title, so we juste print the id and the text
-					if section_title_text == 'N/A':
-						print(f'{section_id}: {section_content_text}')
-					else:
-						print(f'{section_id}: {section_title_text} - {section_content_text}')
-					print([document_type_text, eduskunta_id_text, date_id_text, law_title_text, section_id, section_title_text, section_content_text, lst_of_signatories[0][0], lst_of_signatories[0][1],lst_of_signatories[1][0],lst_of_signatories[1][1]])
-					writer.writerow([document_type_text, eduskunta_id_text, date_id_text, law_title_text, section_id, section_title_text, section_content_text, lst_of_signatories[0][0], lst_of_signatories[0][1],lst_of_signatories[1][0],lst_of_signatories[1][1]])
+					lst_of_signatories.append([position_text,name_text])
+					lst_of_signatories = remove_newlines(lst_of_signatories)
+					#The list will be like this  [[postion_first, name_first], [position_second,name_second]]
+					print(f'{position_text}: {name_text}')
+			else:
+				lst_of_signatories = [['N/A','N/A'],['N/A','N/A']]
 			
+			#Extract the section of the law
+			sections = saados.findall('saa:Pykalisto/saa:Pykala', namespaces)
+			if sections ==[]:
+				sections = saados.findall('saa:Pykalisto/saa:Luku/saa:Pykala', namespaces)
+			#print(sections)
+			for section in sections:
+				#Search all the information about a section
+				#The section here contain the main text of an article
+
+				section_id = section.get('{http://www.vn.fi/skeemat/saadoselementit/2010/04/27}identifiointiTunnus')
+				section_title = section.find('saa:SaadosOtsikkoKooste', namespaces)
+				section_title_text = section_title.text if section_title is not None else 'N/A'
+			
+				#Concat the content of the tag MomenttiKooste
+				section_content_parts = []
+				momentti_kooste = section.findall('saa:MomenttiKooste', namespaces)
+				collect_text(momentti_kooste, section_content_parts)
+				
+				khodat_momentti = section.findall('saa:KohdatMomentti', namespaces)
+				for division in khodat_momentti:
+					momentti_johdanto_kooste = division.findall('saa:MomenttiJohdantoKooste', namespaces)
+					collect_text(momentti_johdanto_kooste , section_content_parts)
+				
+					momentti_kohta_kooste = division.findall('saa:MomenttiKohtaKooste', namespaces)
+					for momentti in momentti_kohta_kooste:
+						if momentti is not None and momentti.text is not None:
+							print(f'{momentti.text} Ceci est un texte')
+							text = momentti.text
+							section_content_parts.append(str(text).strip())	
+							print(section_content_parts)
+					#get the data in Luku Tag
+					#In some articles, there is Luku tag that prevent the retrieving of others tags included in the luku tag
+					# Extract sections and chapters
+				for luku in saados.findall('saa:Luku', namespaces):
+						luku_id = luku.get('{http://www.vn.fi/skeemat/saadoselementit/2010/04/27}identifiointiTunnus', 'N/A')
+						luku_title = luku.findtext('saa:SaadosOtsikkoKooste', default='N/A', namespaces=namespaces)
+
+						for section in luku.findall('saa:Pykala', namespaces):
+								section_id = section.get('{http://www.vn.fi/skeemat/saadoselementit/2010/04/27}identifiointiTunnus', 'N/A')
+								section_title_text = section.findtext('saa:SaadosOtsikkoKooste', default='N/A', namespaces=namespaces)
+								
+								# Concat the content of the tag MomenttiKooste
+								#section_content_parts = []
+								momentti_kooste = section.findall('saa:MomenttiKooste', namespaces)
+								collect_text(momentti_kooste, section_content_parts)
+								#Test print
+								print(chapter_content_parts, "zehtgequirghvsudgfhestuhgbudfghvdufhbqiufdhg")	   
+				section_content_text = ' '.join(section_content_parts) if section_content_parts else 'N/A'
+				print(type(section_content_text))
+				print(section_content_text)
+				section_content_text =  section_content_text.replace(";", "")
+				#Sometimes there is no section title, so we juste print the id and the text
+				if section_title_text == 'N/A':
+					print(f'{section_id}: {section_content_text}')
+				else:
+					print(f'{section_id}: {section_title_text} - {section_content_text}')
+				print([document_type_text, eduskunta_id_text, date_id_text, law_title_text, section_id, section_title_text, section_content_text, lst_of_signatories[0][0], lst_of_signatories[0][1],lst_of_signatories[1][0],lst_of_signatories[1][1]])
+				writer.writerow([document_type_text, eduskunta_id_text, date_id_text, law_title_text, section_id, section_title_text, section_content_text, lst_of_signatories[0][0], lst_of_signatories[0][1],lst_of_signatories[1][0],lst_of_signatories[1][1]])
+		
 		
 
 
 
 def main():
 
-	#Part for the creation of a CSV of one particular year data
+	#---------------Part for the creation of a CSV of one particular year data---------------------------
 	
-	year_id= '2015'
+	"""year_id= 'Finlex_processing/2015'
 	count = 0 
 	#in case, we want to debug
 	create_csv(year_id+"_output.csv")
@@ -181,8 +184,8 @@ def main():
 		show_xml_info(file_path, year_id+"_output.csv")
 			#count+=1 
 		
-	
-	#Part for the creation of a data csv that contain all the data between 1917 and 2023
+	"""
+	#----------------Part for the creation of a data csv that contain all the data between 1917 and 2023------------------
 	
 	"""create_csv("output.csv")
 	for number in range(1918,2024):
@@ -194,7 +197,8 @@ def main():
 			show_xml_info(file_path, "output.csv")
 	
 	"""
-	show_xml_info("2015/asd20150052.xml", "2015_52_output.csv")
+	#Creation of a specific CSV for the article 52 of 2015
+	show_xml_info("Finlex_processing/2015/asd20150052.xml", "Finlex_processing/2015_52_output.csv")
 
 	
 #MAIN PROGRAM
